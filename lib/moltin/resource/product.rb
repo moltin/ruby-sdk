fc598f8317ba97f87f7fc8d7a8e9b8fd9326358e
Marc Qualie <marc@marcqualie.com>
2015-05-02 22:22:25 +0100

    Custom image helper

--- lib/moltin/resource/product.rb
+++ lib/moltin/resource/product.rb
@@ -3,6 +3,10 @@ require 'moltin/api/crud_resource'
 module Moltin
   module Resource
     class Product < Moltin::Api::CrudResource
+      def image_url
+        return nil unless images.any?
+        images[0]['url']['https']
+      end
     end
   end
 end
