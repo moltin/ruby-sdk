a7d5340c4b34cf8337f2d1b8f6911e97238f4889
Marc Qualie <marc@marcqualie.com>
2015-05-02 14:47:03 +0100

    Configure rspec

--- spec/moltin_spec.rb
+++ spec/moltin_spec.rb
@@ -1,11 +1,7 @@
-require 'spec_helper'
+require 'moltin'
 
 describe Moltin do
   it 'has a version number' do
     expect(Moltin::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'moltin'
+
+require 'rspec/expectations'
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec
+
+end
