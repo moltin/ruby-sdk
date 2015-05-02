643838105d2b5bd757d63c9018de8a52203c7966
Marc Qualie <marc@marcqualie.com>
2015-05-02 15:26:18 +0100

    Allow api host to be changed

--- lib/moltin/config.rb
+++ lib/moltin/config.rb
@@ -1,9 +1,11 @@
 module Moltin
   class Config
 
+    @api_host = 'api.moltin.com'
     @api_version = '1.0'
 
     class << self
+      attr_accessor :api_host
       attr_accessor :api_version
       attr_accessor :api_client_id
       attr_accessor :api_client_secret
--- spec/lib/moltin/config_spec.rb
+++ spec/lib/moltin/config_spec.rb
@@ -14,6 +14,16 @@ describe Moltin::Config do
       end
     end
 
+    describe "#api_host" do
+      it "has default value" do
+        expect(Moltin::Config.api_host).to eq 'api.moltin.com'
+      end
+      it "can be updated" do
+        Moltin::Config.api_host = 'api.moltin.dev'
+        expect(Moltin::Config.api_host).to eq 'api.moltin.dev'
+      end
+    end
+
     describe "#api_client_id" do
       it "has no default value" do
         expect(Moltin::Config.api_client_id).to eq nil
