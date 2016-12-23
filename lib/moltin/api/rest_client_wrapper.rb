72a3af8bb851ff5f5d7432a492e536295a8e9a2e
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-12-23 15:17:42 +0100

    Refactore RestClientWrapper (follow Rubocop advices)

--- lib/moltin/api/rest_client_wrapper.rb
+++ lib/moltin/api/rest_client_wrapper.rb
@@ -1,38 +1,38 @@
 require 'moltin/api/request'
 require 'moltin/api/rest_client_wrapper'
+module Moltin
+  module Api
+    class RestClientWrapper
+      def initialize(path, custom_headers = {})
+        endpoint  = Moltin::Api::Request.build_endpoint(path)
+        headers   = Moltin::Api::Request.headers(custom_headers)
+        params    = { verify_ssl: OpenSSL::SSL::VERIFY_NONE, headers: headers }
+        @instance = RestClient::Resource.new(endpoint, params)
+      end
 
-class Moltin::Api::RestClientWrapper
-  def initialize(path, custom_headers = {})
-    @instance = RestClient::Resource.new(
-      Moltin::Api::Request.build_endpoint(path),
-      {
-        verify_ssl: OpenSSL::SSL::VERIFY_NONE,
-        headers: Moltin::Api::Request.headers(custom_headers),
-      }
-    )
-  end
+      def get
+        @instance.get do |response|
+          yield response
+        end
+      end
 
-  def get
-    @instance.get do |response|
-      yield response
-    end
-  end
+      def post(data = {})
+        @instance.post data do |response|
+          yield response
+        end
+      end
 
-  def post data
-    @instance.post data do |response|
-      yield response
-    end
-  end
+      def put(data = {})
+        @instance.put data do |response|
+          yield response
+        end
+      end
 
-  def put data
-    @instance.put data do |response|
-      yield response
-    end
-  end
-
-  def delete
-    @instance.delete do |response|
-      yield response
+      def delete
+        @instance.delete do |response|
+          yield response
+        end
+      end
     end
   end
 end
