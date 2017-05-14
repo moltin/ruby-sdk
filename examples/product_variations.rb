538fe7479f412f0a2edcf1026531ee54fb7beff6
T-Dnzt <thi.denizet@gmail.com>
2017-05-14 18:26:19 +0700

    Fix configuration types

--- examples/product_variations.rb
+++ examples/product_variations.rb
@@ -2,7 +2,7 @@ require_relative 'init'
 
 # To show the request logs, simply replace the client instantiation
 # with moltin = Moltin::Client.new(logger: Logger.new(STDOUT))
-moltin = Moltin::Client.new(logger: Logger.new(STDOUT))
+moltin = Moltin::Client.new
 
 begin
   ap '---------------------------'
